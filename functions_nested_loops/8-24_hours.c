a74c88f0bffb39bbfc62020f7e92a59821ae691e
Antoine-evs <antoine.evesque@gmail.com>
2023-10-16 06:04:09 -0700

    8-24_hours.c

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * jack_bauer - print from 00:00 to 23:59
+ *
+ * Return: void
+ */
+
+void jack_bauer(void)
+{
+	int h;
+	int min;
+
+	h = 0;
+	while (h < 24)
+	{
+		min = 0;
+		while (min < 60)
+		{
+			_putchar(h / 10 + '0');
+			_putchar(h % 10 + '0');
+			-putchar(':');
+			_putchar(min / 10 + '0');
+			_putchar(min % 10 + '0');
+			min++;
+		}
+		h++;
+	}
+
+}
