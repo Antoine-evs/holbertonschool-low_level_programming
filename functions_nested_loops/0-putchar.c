63089d173f7affaebc21275bd9857d13e1750576
Antoine-evs <antoine.evesque@gmail.com>
2023-10-16 04:30:54 -0700

    0-putchar.c and README

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char *str;
+	int i;
+
+	str = "_putchar\n";
+	i = 0;
+	while(str[i])
+		putchar(str[i++]);
+	return (0);
+}
---
+++ functions_nested_loops/README.md
@@ -0,0 +1,2 @@
+16/10 13:30
+c- Functions, nested loops
