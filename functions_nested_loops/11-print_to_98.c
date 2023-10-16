4f0763eade9869ea428edb48ac27c0e465691887
Antoine-evs <antoine.evesque@gmail.com>
2023-10-16 08:36:42 -0700

    11-print_to_98.c

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -1,26 +1,22 @@
 #include "main.h"
+#include <stdio.h>
+
 /**
- * print_to 98 - print to 98
+ * print_to_98 - print to 98
  *
  * Return: void
  */
 
 void print_to_98(int n)
 {
-	while (n >= 98)
-	{
-		_putchar(n + '0');
-		_putchar(',');
-		_putchar(' ');
-		n--;
-	}
-	while (n <= 98)
-	{
-		if (n < 0)
-			_putchar((n * - 1) +'0');
-		_putchar(n + '0');
-		_putchar(',');
-		_putchar(' ');
-		n++;
-	}
+	while (n < 98)
+                printf("%d, ", n++);
+	while (n > 98)
+		printf("%d, ", n--);
+	printf("98\n");
+}
+
+int main(void)
+{
+	print_to_98(64);
 }
