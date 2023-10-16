15cef7ffe7bbb5690372014dfa0434f09bdda7ea
Antoine-evs <antoine.evesque@gmail.com>
2023-10-16 05:40:42 -0700

    6-abs.c

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs - absolute value
+ * @n : int to evaluate
+ *
+ * Return: absolute value 
+ */
+
+int _abs(int n)
+{
+	if (n < 0)
+		return (n * -1);
+	return (n);
+}
